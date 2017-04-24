7f3fc559754fde3257fa70dc5b40b918dd269322
smallFoxHtw <2249382610@qq.com>
2017-04-24 21:46:04 +0800

    Initial commit

---
+++ app/src/main/java/com/fedming/bottomnavigationdemo/Frag2.java
@@ -0,0 +1,22 @@
+package com.fedming.bottomnavigationdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Htw on 2017/4/24.
+ */
+
+public class Frag2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.frag_layout2,container,false);
+        return  view;
+    }
+}
